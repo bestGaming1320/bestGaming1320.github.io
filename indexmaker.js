2831ae5064947867a1b3115799e67427461c9e6f
bestGaming1320 <59489532+bestGaming1320@users.noreply.github.com>
2021-12-01 19:01:25 +0100

    improvements

---
+++ indexmaker.js
@@ -0,0 +1,6 @@
+function init() {
+    if (navigator.userAgent.includes("Firefox")) {
+        document.body.style.justifyContent = "center";
+        document.body.style.display = "grid";
+    }
+}
