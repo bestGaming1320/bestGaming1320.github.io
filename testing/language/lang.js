05234d6dd7efa12c2b6bd30e60b925cab1f06303
bestGaming1320 <59489532+bestGaming1320@users.noreply.github.com>
2021-12-23 19:13:03 +0100

    created a script to change the language

---
+++ testing/language/lang.js
@@ -0,0 +1,40 @@
+﻿var langs = {
+    "langindex": {
+        "en": {
+            "p0": "Text of p0",
+            "e1": "Text of e1"
+        },
+        "de": {
+            "p0": "Text von p0",
+            "e1": "Text von e1"
+        }
+    },
+    "settings": {
+        "en": {
+            "langSel": "en",
+            "settP": "Settings",
+            "langP": "Language",
+            "infoP": "Currently only works on this page"
+        },
+        "de": {
+            "langSel": "de",
+            "settP": "Einstellungen",
+            "langP": "Sprache",
+            "infoP": "Funktioniert aktuell nur auf dieser Seite"
+        }
+    },
+    "pageName": {
+        "en": {},
+        "de": {}
+    }
+}
+var cl = localStorage.getItem("lang")
+function elm(n) { return document.getElementById(n) }
+function setL(inp,pg) {
+    for (i = 0; i < inp.length; i++) {
+        let val = langs[pg][cl][inp[i]]
+        let tN = elm(inp[i]).tagName
+        if (tN == "P") elm(inp[i]).innerHTML = val;
+        else if (tN == "SELECT") elm(inp[i]).value = val
+    }
+}
