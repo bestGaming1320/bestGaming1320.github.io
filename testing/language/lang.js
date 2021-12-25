f43697b675f659b25c2d10806c069dc519944cc1
bestGaming1320 <59489532+bestGaming1320@users.noreply.github.com>
2021-12-25 16:07:22 +0100

    added title and description change

--- testing/language/lang.js
+++ testing/language/lang.js
@@ -1,26 +1,18 @@
 ﻿var langs = {
     "langindex": {
         "en": {
+            "metaTitle": "Language Script Testing",
+            "metaDesc": "This is a page to test the language script",
+            "title": "Language Script Testing",
             "p0": "Text of p0",
             "e1": "Text of e1"
         },
         "de": {
+            "metaTitle": "Sprachskript Test",
+            "metaDesc": "Dies ist eine Seite um den Sprachskript zu testen",
+            "title": "Sprachskript Test",
             "p0": "Text von p0",
             "e1": "Text von e1"
-        }
-    },
-    "settings": {
-        "en": {
-            "langSel": "en",
-            "settP": "Settings",
-            "langP": "Language",
-            "infoP": "Currently only works on this page"
-        },
-        "de": {
-            "langSel": "de",
-            "settP": "Einstellungen",
-            "langP": "Sprache",
-            "infoP": "Funktioniert aktuell nur auf dieser Seite"
         }
     },
     "pageName": {
@@ -30,11 +22,13 @@
 }
 var cl = localStorage.getItem("lang")
 function elm(n) { return document.getElementById(n) }
-function setL(inp,pg) {
+function setL(inp, pg) {
+    document.title = langs[pg][cl].metaTitle;
+    document.querySelector('meta[name="description"]').setAttribute("content", langs[pg][cl].metaDesc);
     for (i = 0; i < inp.length; i++) {
         let val = langs[pg][cl][inp[i]]
         let tN = elm(inp[i]).tagName
-        if (tN == "P") elm(inp[i]).innerHTML = val;
+        if (tN == "P"||tN=="H2") elm(inp[i]).innerHTML = val;
         else if (tN == "SELECT") elm(inp[i]).value = val;
         else alert("Unsupported Element: "+tN)
     }
