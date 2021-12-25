ad8c15ac215bfefa03b4a911650273b2c4ea78e2
bestGaming1320 <59489532+bestGaming1320@users.noreply.github.com>
2021-12-25 15:07:47 +0100

    made lang script for index

---
+++ lang.js
@@ -0,0 +1,48 @@
+﻿var langs = {
+    "settings": {
+        "en": {
+            "langSel": "en",
+            "settP": "Settings",
+            "langP": "Language",
+            "infoP": "Currently only works on this page"
+        },
+        "de": {
+            "langSel": "de",
+            "settP": "Einstellungen",
+            "langP": "Sprache",
+            "infoP": "Funktioniert aktuell nur auf dieser Seite"
+        }
+    },
+    "mIndex": {
+        "en": {
+            "title": "Select a page",
+            "tool": "Tools",
+            "game": "Games",
+            "colSc": "Color Screen",
+            "clock": "Clock",
+            "book": "Book",
+            "docs": "Documentations"
+        },
+        "de": {
+            "title": "Suche eine Seite aus",
+            "tool": "Tools",
+            "game": "Spiele",
+            "colSc": "Farbbildschirm",
+            "clock": "Uhr",
+            "book": "Buch",
+            "docs": "Dokumentationen"
+        }
+    }
+}
+if (localStorage.getItem("lang") == null) localStorage.setItem("lang", "en")
+var cl = localStorage.getItem("lang")
+function elm(n) { return document.getElementById(n) }
+function setL(inp, pg) {
+    for (i = 0; i < inp.length; i++) {
+        let val = langs[pg][cl][inp[i]]
+        let tN = elm(inp[i]).tagName
+        if (tN == "P" || tN == "BUTTON") elm(inp[i]).innerHTML = val;
+        else if (tN == "SELECT") elm(inp[i]).value = val;
+        else alert("Unsupported Element: " + tN)
+    }
+}
