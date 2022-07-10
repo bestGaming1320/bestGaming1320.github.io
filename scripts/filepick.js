971ad8cf3a949e84414d622be552b965369f5d38
bestGaming1320 <59489532+bestGaming1320@users.noreply.github.com>
2022-07-10 03:05:05 +0200

    some changes, new page
    - new page "Image Math" -> allows to input an image and perform a math operation using pixels of the image
    - renamed settings to preferences for future use and added unusable themes option
    - added ability to print generated things to dummy file generator and added option to generate csv files
    - language data and script are now separate files
    - removed old code from random hex color generator, improved visibility of selected hex code and added possibility to generate rgb codes
    - improved code of color converter and made it easier to use
    - updated file picker element

--- scripts/filepick.js
+++ scripts/filepick.js
@@ -1,20 +1,34 @@
 "use strict";
 class filePicker extends HTMLElement {
+    shd; //shadow dom
+    enableStatus; //if file picker can be used or not
     connectedCallback() {
-        const shd = this.attachShadow({ mode: "open" });
+        this.shd = this.attachShadow({ mode: "open" });
+        this.enableStatus = this.getAttribute("data-enabled") ? JSON.parse(this.getAttribute("data-enabled")) : true
+        this.enabled(this.enableStatus)
         const div = document.createElement("div");
         const inp = document.createElement("input");
         const p = document.createElement("p");
         let config = JSON.parse(this.getAttribute("data-config"));
-        config = config ? config : { "center": true, "centerEl": false, "multi": false, "fixedH": false, "accept": "*", "remEmpt": true };
+        config = config ? config : { "center": true, "centerEl": false, "multi": false, "fixedH": false, "accept": "*", "remEmpt": true, "custText": false, "custSelText": false };
 
         const modEl = fileList => {
             this.dispatchEvent(new CustomEvent("filepick", { detail: { files: fileList } }));
-            if (config.multi) p.innerHTML = "Selected files: <br>";
-            else p.innerHTML = "Selected file: ";
+            changeSelTxt()
             for (let i = 0; i < fileList.length; i++) {
                 p.innerHTML += fileList[i].name + (config.center ? "<br>" : ", ");
             }
+        }
+
+        function changeTxt() {
+            if (config.custText) p.innerHTML = config.custText
+            else if (config.multi) p.innerHTML = "Click here to pick files"
+            else p.innerHTML = "Click here to pick a file"
+        }
+        function changeSelTxt() {
+            if (config.custSelText) return config.custSelText
+            else if (config.multi) p.innerHTML = "Selected files:<br>"
+            else p.innerHTML = "Selected file:<br>"
         }
 
         if (!this.style.height) {
@@ -24,12 +38,11 @@ class filePicker extends HTMLElement {
         }
         if (!this.style.width) this.style.width = "220px";
 
-        if (config.multi) p.innerHTML = "Click here to pick files";
-        else p.innerHTML = "Click here to pick a file";
+        changeTxt()
         p.style.color = "#fff";
         div.appendChild(p);
         div.appendChild(inp);
-        shd.appendChild(div);
+        this.shd.appendChild(div);
 
         inp.type = "file";
         inp.accept = config.accept
@@ -39,12 +52,11 @@ class filePicker extends HTMLElement {
             this.setAttribute("value", inp.value)
             if (inp.files.length > 0) modEl(inp.files)
             else if (config.remEmpt) {
-                if (config.multi) p.innerHTML = "Click here to pick files";
-                else p.innerHTML = "Click here to pick a file";
+                changeTxt()
             }
         }
 
-        div.onclick = () => inp.click();
+        div.onclick = () => { if (this.enableStatus) inp.click() };
         div.style.height = this.style.height;
         div.style.minHeight = this.style.minHeight;
         div.style.width = this.style.width;
@@ -52,6 +64,23 @@ class filePicker extends HTMLElement {
         div.style.background = "#333";
         div.style.border = "2px solid #888";
         if (config.centerEl) div.style.margin = "auto";
+    }
+    enabled(b) {
+        this.enableStatus = b;
+        if (b) {
+            this.style.filter = "brightness(1)"
+        } else {
+            this.style.filter = "brightness(0.7)"
+        }
+    }
+    changeStyle(style) {
+        if (style.height) div.style.height = style.height;
+        if (style.minHeight) div.style.minHeight = style.minHeight;
+        if (style.width) div.style.width = style.width;
+        if (style.minWidth) div.style.minWidth = style.minWidth;
+        if (style.background) div.style.background = style.background;
+        if (style.border) div.style.border = style.border;
+        if (style.margin) div.style.margin = style.margin;
     }
 }
 
--- scripts/lang.js
+++ scripts/lang.js
@@ -1,56 +1,22 @@
-var langs = {
-    "settings": {
-        "en": {
-            "metaTitle": "Settings",
-            "metaDesc": "Settings for the website",
-            "langSel": "en",
-            "settP": "Settings",
-            "langP": "Language",
-            "infoP": "Currently only works on these pages:<br>Settings<br>Homepage"
-        },
-        "de": {
-            "metaTitle": "Einstellungen",
-            "metaDesc": "Einstellungen für die Website",
-            "langSel": "de",
-            "settP": "Einstellungen",
-            "langP": "Sprache",
-            "infoP": "Funktioniert aktuell nur auf diesen Seiten:<br>Einstellungen<br>Homepage"
-        }
-    },
-    "mIndex": {
-        "en": {
-            "metaTitle": "Homepage",
-            "metaDesc": "Links to all my pages",
-            "title": "Select a page",
-            "tool": "Tools",
-            "game": "Games",
-            "colSc": "Color Screen",
-            "clock": "Clock",
-            "book": "Book",
-            "docs": "Documentations"
-        },
-        "de": {
-            "metaTitle": "Homepage",
-            "metaDesc": "Links zu all meinen Seiten",
-            "title": "Suche eine Seite aus",
-            "tool": "Tools",
-            "game": "Spiele",
-            "colSc": "Farbbildschirm",
-            "clock": "Uhr",
-            "book": "Buch",
-            "docs": "Dokumentationen"
-        }
-    }
-}
-function hasV(el) {
-    if (elm(el).tagName == "BUTTON") return false
-    if (elm(el).value == undefined) return false
-    else return true
-}
+var langs;
 if (localStorage.getItem("lang") == null) localStorage.setItem("lang", "en")
 var cl = localStorage.getItem("lang")
-function elm(n) { return document.getElementById(n) }
+
+function elm(n) {
+    return document.getElementById(n)
+}
+
 function setL(inp, pg) {
+    fetch("/scripts/langs.json").then(resp => {
+        if (resp.ok) return resp.json()
+        else console.error("There was an error getting the language file. Status: " + resp.status)
+    }).then(dat => {
+        langs = dat;
+        setLang(inp, pg)
+    })
+}
+
+function setLang(inp, pg) {
     document.title = langs[pg][cl].metaTitle;
     document.querySelector('meta[name="description"]').setAttribute("content", langs[pg][cl].metaDesc);
     for (i = 0; i < inp.length; i++) {
@@ -58,4 +24,10 @@ function setL(inp, pg) {
         if (hasV(inp[i])) elm(inp[i]).value = val;
         else elm(inp[i]).innerHTML = val;
     }
+}
+
+function hasV(el) {
+    if (elm(el).tagName == "BUTTON") return false
+    if (elm(el).value == undefined) return false
+    else return true
 }
